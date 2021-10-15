0b74e84352640231fda93c15b36e094a503024bf
Michael Hardcastle <maaicle@gmail.com>
2021-10-15 10:30:11 -0600

    completed arryas.js

--- arrays.js
+++ arrays.js
@@ -4,12 +4,14 @@
 const faveColors = ['red', 'green', 'black']
 
 //CODE HERE
+let colorCopy = faveColors.slice(0, 3); //This is a weired one, I thought my arguments would need to be (0, 2) but that only returned the first two colors. 
 
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
-
+colorCopy.push('purple');
+// console.log(colorCopy);
 //////////////////PROBLEM 3////////////////////
 /*
   Using an array method, create a new array called 'middleNums' from the 
@@ -19,6 +21,10 @@ const faveColors = ['red', 'green', 'black']
 const numbers = [1, 2, 3, 4, 5]
 
 //CODE HERE
+let middleNums = numbers.slice(1, 4);
+// let middleNums = numbers.splice(1, 3); // Not sure if I should use the splice or slice method so I did both. 
+// console.log(numbers);
+// console.log(middleNums);
 
 
 //////////////////PROBLEM 4////////////////////
@@ -35,3 +41,19 @@ const numbers = [1, 2, 3, 4, 5]
 */
 
 // CODE HERE
+function bigOrSmall (arr) {
+  let answers = [];
+  for (i = 0; i < arr.length; i++) {
+    if(arr[i] > 100) {
+      answers.push('big');
+    } else {
+      answers.push('small');
+    }
+  }
+  return answers
+};
+let questions = [4000, 211, 99, 100, 101];
+let functionAnswer = bigOrSmall(questions);
+
+console.log(questions);
+console.log(functionAnswer);
